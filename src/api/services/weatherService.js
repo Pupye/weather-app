866eb035eed9492c020c5f57b45fa8eaf3d1bf1c
Pupye <fullhouseya89@gmail.com>
2020-09-16 23:16:35 +0600

    fix service

--- src/api/services/weatherService.js
+++ src/api/services/weatherService.js
@@ -5,6 +5,10 @@ async function getWeatherInfoForCity(city, weatherDAO) {
     if (!info) {
         throw new NoRecordsFoundError('no records found for city');
     }
+    //converting string to int
+    info["today"] = parseInt(info["today"], 10);
+    info["yesterday"] = parseInt(info["yesterday"], 10);
+    info["week-average"] = parseInt(info["week-average"], 10);
     return info;
 }
 
