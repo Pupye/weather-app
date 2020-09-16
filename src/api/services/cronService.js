6294b3df3e895168ed9c66293ed98c885d037187
Pupye <fullhouseya89@gmail.com>
2020-09-16 16:53:57 +0600

    small fix on cron service

--- src/api/services/cronService.js
+++ src/api/services/cronService.js
@@ -2,14 +2,14 @@ import { CronJob } from 'cron'
 import logger from '../config/logger';
 import weatherDAO from '../repository/weatherDAO'
 
-const refreshMaterializedViewsJob = new CronJob('0 0 0 * * *', async () => {
+const refreshMaterializedViewsJob = new CronJob('00 00 00 * * *', async () => {
     try {
         logger.info("refreshing materialized views")
         await weatherDAO.refreshWeekWeatherMaterializedView()
     } catch (error) {
         logger.error(error.stack)
     }
-}, null, true, 'Europe/London')
+})
 
 async function startJobs() {
     refreshMaterializedViewsJob.start()
